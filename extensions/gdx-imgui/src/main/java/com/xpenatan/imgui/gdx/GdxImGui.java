7a81d05966585266cc29609708716062952759c8
Natan <xpenatan@gmail.com>
2019-05-06 13:22:20 -0300

    Removed test methods

--- extensions/gdx-imgui/src/main/java/com/xpenatan/imgui/gdx/GdxImGui.java
+++ extensions/gdx-imgui/src/main/java/com/xpenatan/imgui/gdx/GdxImGui.java
@@ -21,40 +21,4 @@ public class GdxImGui extends ImGui<GdxDrawData> {
 	public GdxImGui() {
 		super(new GdxDrawData());
 	}
-
-
-	@Deprecated
-	public void getDrawDataOld() {
-		renderDrawData(0);
-		int size = drawData.cmdListsCount;
-
-		for(int i = 1; i < size; i++) {
-			renderDrawData(i);
-		}
-	}
-
-	@Deprecated
-	public void renderDrawData() {
-		drawData.render();
-	}
-
-	@Deprecated
-	public void renderDrawData(int index) {
-		ByteBuffer cmdByteBuffer = drawData.cmdByteBuffer;
-		ByteBuffer vByteBuffer = drawData.vByteBuffer;
-		ByteBuffer iByteBuffer = drawData.iByteBuffer;
-
-		vByteBuffer.position(0);
-		vByteBuffer.limit(0);
-		iByteBuffer.position(0);
-		iByteBuffer.limit(0);
-		cmdByteBuffer.position(0);
-		cmdByteBuffer.limit(0);
-		ImGuiNative.getDrawData(index, drawData, iByteBuffer, vByteBuffer, cmdByteBuffer);
-
-		vByteBuffer.limit(drawData.vtxCount * DrawData.vBufferSize); // 4 color floats
-		iByteBuffer.limit(drawData.idxCount * DrawData.iBufferSize);
-		cmdByteBuffer.limit(drawData.cmdCount * DrawData.cmdBufferSize);
-		drawData.render(null);
-	}
 }
