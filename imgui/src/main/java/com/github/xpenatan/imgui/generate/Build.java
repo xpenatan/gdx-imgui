fbcd1f91c692afcd1242470d3f74418bab84ac0b
Natan <xpenatan@gmail.com>
2020-01-06 19:09:55 -0200

    test

--- imgui/src/main/java/com/github/xpenatan/imgui/generate/Build.java
+++ imgui/src/main/java/com/github/xpenatan/imgui/generate/Build.java
@@ -62,10 +62,10 @@ public class Build {
 		new NativeCodeGenerator().generate("src/main/java",classpathStr + File.pathSeparator, path + "/jni");
 		new AntScriptGenerator().generate(buildConfig, win64, lin64, mac64);
 
-		BuildExecutor.executeAnt("jni/build-windows64.xml", "-v", "-Dhas-compiler=true", "clean", "postcompile");
+//		BuildExecutor.executeAnt("jni/build-windows64.xml", "-v", "-Dhas-compiler=true", "clean", "postcompile");
 //		BuildExecutor.executeAnt("jni/build-linux64.xml", "-v", "-Dhas-compiler=true", "clean", "postcompile");
 //		BuildExecutor.executeAnt("jni/build-macosx64.xml", "-v", "-Dhas-compiler=true");
-		BuildExecutor.executeAnt("jni/build.xml", "-v", "pack-natives");
+//		BuildExecutor.executeAnt("jni/build.xml", "-v", "pack-natives");
 	}
 
 	public static void copyDir(Path src, Path dest, String... excludes) throws IOException {
