aac59ca38461a4b96f946fd1120a3f0b3da47038
Natan <xpenatan@gmail.com>
2021-06-26 15:36:50 -0300

    fix build error

--- imgui-core/src/main/java/com/github/xpenatan/imgui/jnicode/ImGuiInternalNative.java
+++ imgui-core/src/main/java/com/github/xpenatan/imgui/jnicode/ImGuiInternalNative.java
@@ -13,7 +13,7 @@ public class ImGuiInternalNative {
 	*/
 
 	public static native void ItemSize(float x1, float y1, float text_offset_y) /*-{ }-*/; /*
-		ImGui::ItemSize(ImVec2(x1, y1, x2, y2), text_offset_y);
+		ImGui::ItemSize(ImVec2(x1, y1), text_offset_y);
 	*/
 
 	public static native void ItemSize(float x1, float y1, float x2, float y2, float text_offset_y) /*-{ }-*/; /*
