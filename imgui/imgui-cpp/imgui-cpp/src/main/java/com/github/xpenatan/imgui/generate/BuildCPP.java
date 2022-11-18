13a0b886dfab3c3be08a2c104364141510145e0a
Natan <xpenatan@gmail.com>
2022-11-18 18:35:11 -0300

    Use file helper

--- imgui/imgui-cpp/imgui-cpp/src/main/java/com/github/xpenatan/imgui/generate/BuildCPP.java
+++ imgui/imgui-cpp/imgui-cpp/src/main/java/com/github/xpenatan/imgui/generate/BuildCPP.java
@@ -1,6 +1,7 @@
 package com.github.xpenatan.imgui.generate;
 
 import com.github.xpenatan.jparser.cpp.CPPBuildHelper;
+import com.github.xpenatan.jparser.cpp.FileCopyHelper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
@@ -19,45 +20,9 @@ public class BuildCPP {
 
         File from = new File(cppPath + "/cpp-source/");
         File dest = new File(cppPath + "/jni/src");
-        BuildCPP.copyDir(from.toPath(), dest.toPath());
+        FileCopyHelper.copyDir(from.toPath(), dest.toPath());
 
         CPPBuildHelper.build(libName, cppPath);
     }
 
-    public static void copyDir(Path src, Path dest, String... excludes) throws IOException {
-        File directory = dest.toFile();
-        if(!directory.exists())
-            directory.mkdirs();
-
-        if(Files.exists(src)) {
-            Files.walkFileTree(src, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
-                    boolean skip = false;
-                    if(excludes != null) {
-                        String fileStr = path.getFileName().toString();
-                        for(int i = 0; i < excludes.length; i++) {
-                            String excludeFile = excludes[i];
-                            if(fileStr.contains(excludeFile)) {
-                                skip = true;
-                                break;
-                            }
-                        }
-                    }
-                    if(!skip) {
-                        File file = path.toFile();
-                        String name = file.getName();
-                        Path newDest = dest.resolve(name);
-                        Files.copy(path, newDest, StandardCopyOption.REPLACE_EXISTING);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult postVisitDirectory(Path directory, IOException ioException) throws IOException {
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
-    }
 }
