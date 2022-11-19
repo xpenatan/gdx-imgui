19281002fa081282188f4557c06c620f094be690
Natan <xpenatan@gmail.com>
2022-11-18 22:07:53 -0300

    remove cpp imports

--- imgui/imgui-cpp/imgui-cpp/src/main/java/com/github/xpenatan/imgui/generate/BuildCPP.java
+++ imgui/imgui-cpp/imgui-cpp/src/main/java/com/github/xpenatan/imgui/generate/BuildCPP.java
@@ -3,13 +3,6 @@ package com.github.xpenatan.imgui.generate;
 import com.github.xpenatan.jparser.cpp.CPPBuildHelper;
 import com.github.xpenatan.jparser.cpp.FileCopyHelper;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 
 public class BuildCPP {
 
