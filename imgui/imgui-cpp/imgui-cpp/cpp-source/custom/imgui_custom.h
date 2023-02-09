97b7e72cc5d5e7146e7a9cd9431748764fa85939
Natan <xpenatan@gmail.com>
2023-02-09 11:51:18 -0300

    Fix linux build

--- imgui/imgui-cpp/imgui-cpp/cpp-source/custom/imgui_custom.h
+++ imgui/imgui-cpp/imgui-cpp/cpp-source/custom/imgui_custom.h
@@ -2,6 +2,11 @@
 
 #include "imgui.h"
 #include "imgui_internal.h"
+#if defined(_MSC_VER) && _MSC_VER <= 1500 // MSVC 2008 or earlier
+#include <stddef.h>     // intptr_t
+#else
+#include <stdint.h>     // intptr_t
+#endif
 
 class BoolArray {
     public:
