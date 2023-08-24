e82be8fff0d961616cce7551470550db0de97921
Natan <xpenatan@gmail.com>
2023-08-24 00:07:20 -0300

    bk base

---
+++ imgui/base/src/main/java/imgui/ImGui.java
@@ -0,0 +1,43 @@
+package imgui;
+
+import com.github.xpenatan.jparser.loader.JParserLibraryLoader;
+
+/**
+ * @author xpenatan
+ */
+/*[-IDL_SKIP]*/
+public class ImGui {
+
+    /*[-teaVM;-ADD]
+        @org.teavm.jso.JSFunctor
+        public interface OnInitFunction extends org.teavm.jso.JSObject {
+            void onInit();
+        }
+    */
+
+    /*[-teaVM;-REPLACE]
+     public static void init(Runnable run) {
+        JParserLibraryLoader libraryLoader = new JParserLibraryLoader();
+        OnInitFunction onInitFunction = new OnInitFunction() {
+            @Override
+            public void onInit() {
+                run.run();
+            }
+        };
+        setOnLoadInit(onInitFunction);
+        libraryLoader.load("bullet.wasm");
+    }
+    */
+    public static void init(Runnable run) {
+        JParserLibraryLoader libraryLoader = new JParserLibraryLoader();
+        libraryLoader.load("imgui");
+        run.run();
+    }
+
+    /*[-teaVM;-REPLACE]
+        @org.teavm.jso.JSBody(params = { "onInitFunction" }, script = "window.imguiOnInit = onInitFunction;")
+        private static native void setOnLoadInit(OnInitFunction onInitFunction);
+    */
+    /*[-C++;-REMOVE] */
+    public static native void setOnLoadInit();
+}
