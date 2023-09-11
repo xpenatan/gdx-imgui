6d468b87646b84b255c86c0a2a7e4487ca95130d
Natan <xpenatan@gmail.com>
2023-09-11 18:11:12 -0300

    update gradle

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # gdx-imgui
 ![Deploy Libs](https://github.com/xpenatan/gdx-imgui/workflows/Deploy%20Libs/badge.svg)
+[![Sonatype Nexus (Snapshots)](https://img.shields.io/nexus/r/com.github.xpenatan.gdx-imgui/core?nexusVersion=2&server=https%3A%2F%2Foss.sonatype.org&label=release)](https://repo.maven.apache.org/maven2/com/github/xpenatan/gdx-imgui/)
+[![Sonatype Nexus (Snapshots)](https://img.shields.io/nexus/s/com.github.xpenatan.gdx-imgui/core?server=https%3A%2F%2Foss.sonatype.org&label=snapshot)](https://oss.sonatype.org/content/repositories/snapshots/com/github/xpenatan/gdx-imgui/)
+
 
 gdx-imgui is a java binding for C++ [dear-imgui](https://github.com/ocornut/imgui). <br>
 It's meant to be small and close 1-1 to C++. 
---
+++ build.gradle.kts
@@ -0,0 +1,133 @@
+plugins {
+    id("java")
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    val kotlinVersion = "1.8.10"
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects  {
+
+    repositories {
+        mavenLocal()
+        google()
+        mavenCentral()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
+        maven {
+            url = uri("http://teavm.org/maven/repository/")
+            isAllowInsecureProtocol = true
+        }
+    }
+
+    configurations.configureEach {
+        // Check for updates every sync
+        resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+    }
+}
+
+configure(allprojects) {
+    apply {
+        plugin("java")
+    }
+    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.targetCompatibility = JavaVersion.VERSION_11
+}
+
+var libProjects = mutableSetOf(
+    project(":imgui:core"),
+    project(":imgui:desktop"),
+    project(":imgui:teavm"),
+    project(":extensions:gdx")
+//        project(":extensions:lwjgl3"),
+//        project(":extensions:gdx-frame-viewport"),
+//        project(":extensions:imlayout:imlayout-core"),
+//        project(":extensions:imlayout:imlayout-desktop")
+)
+
+configure(libProjects) {
+    apply(plugin = "signing")
+    apply(plugin = "maven-publish")
+
+    group = LibExt.groupId
+    version = LibExt.libVersion
+
+    publishing {
+        repositories {
+            maven {
+                url = uri {
+                    val ver = project.version.toString()
+                    val isSnapshot = ver.uppercase().contains("SNAPSHOT")
+                    val repoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                    val repoUrlSnapshot = "https://oss.sonatype.org/content/repositories/snapshots/"
+                    if (isSnapshot) repoUrlSnapshot else repoUrl
+                }
+                credentials {
+                    username = System.getenv("USER")
+                    password = System.getenv("PASSWORD")
+                }
+            }
+        }
+    }
+
+    tasks.withType<Javadoc> {
+        options.encoding = "UTF-8"
+        (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
+    }
+
+    java {
+        withJavadocJar()
+        withSourcesJar()
+    }
+
+    publishing.publications.configureEach {
+        if (this is MavenPublication) {
+            pom {
+                name.set("gdx-imgui")
+                description.set("Java JNI based binding for imgui")
+                url.set("https://github.com/xpenatan/gdx-imgui")
+                developers {
+                    developer {
+                        id.set("Xpe")
+                        name.set("Natan")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://https://github.com/xpenatan/gdx-imgui.git")
+                    developerConnection.set("scm:git:ssh://https://github.com/xpenatan/gdx-imgui.git")
+                    url.set("http://https://github.com/xpenatan/gdx-imgui/tree/master")
+                }
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+            }
+        }
+    }
+
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    if (signingKey != null && signingPassword != null) {
+        signing {
+            useInMemoryPgpKeys(signingKey, signingPassword)
+            publishing.publications.configureEach {
+                sign(this)
+            }
+        }
+    }
+}
