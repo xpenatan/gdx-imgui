3d3252332a04277ea19f31244c7de7a6d9ca9fee
Natan <xpenatan@gmail.com>
2023-09-17 10:00:19 -0300

    imlayout clean up

--- extensions/imlayout/imlayout-base/src/main/java/imgui/imlayout/ImLayout.java
+++ extensions/imlayout/imlayout-base/src/main/java/imgui/imlayout/ImLayout.java
@@ -5,4 +5,9 @@ public class ImLayout {
         #include "imgui_layout.h"
     */
 
+    public static final int WRAP_PARENT = get_WRAP_PARENT();
+    public static final int MATCH_PARENT = get_MATCH_PARENT();
+
+    public static native int get_WRAP_PARENT();
+    public static native int get_MATCH_PARENT();
 }
