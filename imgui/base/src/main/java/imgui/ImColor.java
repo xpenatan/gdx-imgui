f2b90ef848343d5b45527982e6fb0d072e31d064
Natan <xpenatan@gmail.com>
2023-09-18 08:48:17 -0300

    add missing color methods

--- imgui/base/src/main/java/imgui/ImColor.java
+++ imgui/base/src/main/java/imgui/ImColor.java
@@ -6,7 +6,27 @@ public class ImColor extends ImGuiFloat4 {
         this(1f, 1f, 1f, 1f);
     }
 
-    public ImColor(float x, float y, float z, float w) {
-        super(x, y, z, w);
+    public ImColor(float r, float g, float b, float a) {
+        super(r, g, b, a);
+    }
+
+    public void setColor(float r, float g, float b, float a) {
+        setValue(r, g, b, a);
+    }
+
+    public float getR() {
+        return getX();
+    }
+
+    public float getG() {
+        return getY();
+    }
+
+    public float getB() {
+        return getZ();
+    }
+
+    public float getA() {
+        return getW();
     }
 }
