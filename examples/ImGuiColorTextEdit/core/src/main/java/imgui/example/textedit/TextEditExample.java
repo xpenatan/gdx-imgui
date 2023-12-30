a5cd619d3b5b46dfaaa1ddb8b054a5f7f0c660ad
Natan <xpenatan@gmail.com>
2023-12-30 19:05:00 -0300

    Add more methods

--- examples/ImGuiColorTextEdit/core/src/main/java/imgui/example/textedit/TextEditExample.java
+++ examples/ImGuiColorTextEdit/core/src/main/java/imgui/example/textedit/TextEditExample.java
@@ -3,7 +3,9 @@ package imgui.example.textedit;
 import imgui.ImGui;
 import imgui.example.renderer.ImGuiRenderer;
 import imgui.extension.textedit.Coordinates;
+import imgui.extension.textedit.LanguageDefinition;
 import imgui.extension.textedit.TextEditor;
+import imgui.idl.helper.IDLString;
 
 public class TextEditExample extends ImGuiRenderer {
 
@@ -14,6 +16,22 @@ public class TextEditExample extends ImGuiRenderer {
         super.show();
 
         editor = new TextEditor();
+
+        LanguageDefinition lua = LanguageDefinition.Lua();
+        editor.SetLanguageDefinition(lua);
+
+        IDLString text = new IDLString();
+
+        String code = "\n" +
+                "function onCreate()\n" +
+                "\n" +
+                "end\n" +
+                "\n\n" +
+                "function onRender(delta)\n" +
+                "\n" +
+                "end\n";
+        text.append(code);
+        editor.SetText(text);
     }
 
     @Override
@@ -22,8 +40,7 @@ public class TextEditExample extends ImGuiRenderer {
 
         ImGui.Begin("Editor");
 
-        String text = "\t" + (coordinates.get_mLine() + 1) + "/" + (coordinates.get_mColumn() + 1) + " " + editor.GetTotalLines() + " lines | " + (editor.IsOverwrite() ? "Ovr" : "Ins") + " | " + (editor.CanUndo() ? "*" : " ");
-//                editor.GetLanguageDefinition().mName.c_str(), fileToEdit);
+        String text = "\t" + (coordinates.get_mLine() + 1) + "/" + (coordinates.get_mColumn() + 1) + " " + editor.GetTotalLines() + " lines | " + (editor.IsOverwrite() ? "Ovr" : "Ins") + " | " + (editor.CanUndo() ? "*" : " ") + " | " + editor.GetLanguageDefinition().get_mName().c_str();
         ImGui.Text(text);
 
         editor.Render("Title");
