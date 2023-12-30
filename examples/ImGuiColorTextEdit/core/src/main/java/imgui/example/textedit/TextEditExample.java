deba12dc4eaa93249776710c332a9402477f10f5
Natan <xpenatan@gmail.com>
2023-12-29 22:53:11 -0300

    add wip editor example

--- examples/ImGuiColorTextEdit/core/src/main/java/imgui/example/textedit/TextEditExample.java
+++ examples/ImGuiColorTextEdit/core/src/main/java/imgui/example/textedit/TextEditExample.java
@@ -1,24 +1,32 @@
 package imgui.example.textedit;
 
+import imgui.ImGui;
 import imgui.example.renderer.ImGuiRenderer;
-import imgui.extension.textedit.SelectMode;
+import imgui.extension.textedit.Coordinates;
 import imgui.extension.textedit.TextEditor;
 
 public class TextEditExample extends ImGuiRenderer {
+
+    private TextEditor editor;
 
     @Override
     public void show() {
         super.show();
 
-        TextEditor textEditor = new TextEditor();
-
-        System.out.println("line: " + SelectMode.Line);
-        System.out.println("normal: " + SelectMode.Normal);
-        System.out.println("word: " + SelectMode.Word);
+        editor = new TextEditor();
     }
 
     @Override
     public void renderImGui() {
+        Coordinates coordinates = editor.GetCursorPosition();
 
+        ImGui.Begin("Editor");
+
+        String text = "\t" + (coordinates.get_mLine() + 1) + "/" + (coordinates.get_mColumn() + 1) + " " + editor.GetTotalLines() + " lines | " + (editor.IsOverwrite() ? "Ovr" : "Ins") + " | " + (editor.CanUndo() ? "*" : " ");
+//                editor.GetLanguageDefinition().mName.c_str(), fileToEdit);
+        ImGui.Text(text);
+
+        editor.Render("Title");
+        ImGui.End();
     }
 }
