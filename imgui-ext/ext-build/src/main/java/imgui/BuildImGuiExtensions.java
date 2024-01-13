f2c2871f07258a2f152fdc052bcdac0f008cff7d
Natan <xpenatan@gmail.com>
2024-01-13 18:50:40 -0300

    test

--- imgui-ext/ext-build/src/main/java/imgui/BuildImGuiExtensions.java
+++ imgui-ext/ext-build/src/main/java/imgui/BuildImGuiExtensions.java
@@ -148,7 +148,7 @@ public class BuildImGuiExtensions {
         glueTarget.headerDirs.add("-I" + imguiCppPath + "/src/imgui/");
         glueTarget.headerDirs.add("-I" + imguiCppPath + "/src/jniglue");
         glueTarget.linkerFlags.add(imguiCppPath + "/libs/linux/libimgui64.a");
-
+        glueTarget.linkerFlags.add("-static");
         glueTarget.linkerFlags.add("-static-libgcc");
         glueTarget.linkerFlags.add("-static-libstdc++");
 
