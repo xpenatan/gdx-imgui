22ced20854d49ff5b0c4eb04dc012c6bd40d94ca
Natan <xpenatan@gmail.com>
2024-01-19 08:37:07 -0300

    add missing start on first thread

--- examples/imgui-node-editor/desktop/build.gradle.kts
+++ examples/imgui-node-editor/desktop/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
 dependencies {
     implementation(project(":examples:imgui-node-editor:core"))
 
@@ -21,4 +23,8 @@ tasks.register<JavaExec>("nodeeditor-run-desktop") {
     mainClass.set(mainClassName)
     classpath = sourceSets["main"].runtimeClasspath
     workingDir = assetsDir
+
+    if(DefaultNativePlatform.getCurrentOperatingSystem().isMacOsX) {
+        jvmArgs("-XstartOnFirstThread")
+    }
 }
--- examples/imlayout/desktop/build.gradle.kts
+++ examples/imlayout/desktop/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
 dependencies {
     implementation(project(":examples:imlayout:core"))
 
@@ -21,4 +23,8 @@ tasks.register<JavaExec>("imlayout-run-desktop") {
     mainClass.set(mainClassName)
     classpath = sourceSets["main"].runtimeClasspath
     workingDir = assetsDir
+
+    if(DefaultNativePlatform.getCurrentOperatingSystem().isMacOsX) {
+        jvmArgs("-XstartOnFirstThread")
+    }
 }
