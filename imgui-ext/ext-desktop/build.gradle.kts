295b2439547f57ae2d0444321a0f1fe2dd431183
Natan <xpenatan@gmail.com>
2024-01-18 23:33:59 -0300

    add ext

--- imgui-ext/ext-desktop/build.gradle.kts
+++ imgui-ext/ext-desktop/build.gradle.kts
@@ -2,10 +2,14 @@ val moduleName = "imgui-ext-desktop"
 
 val windowsFile = "$projectDir/../../imgui/imgui-build/build/c++/libs/windows/ext/imgui64.dll"
 val linuxFile = "$projectDir/../../imgui/imgui-build/build/c++/libs/linux/ext/libimgui64.so"
+val macArmFile = "$projectDir/../imgui-build/build/c++/libs/mac/ext/arm/libimguiarm64.dylib"
+val macFile = "$projectDir/../imgui-build/build/c++/libs/mac/ext/libimgui64.dylib"
 
 tasks.jar {
     from(windowsFile)
     from(linuxFile)
+    from(macFile)
+    from(macArmFile)
 }
 
 java {
