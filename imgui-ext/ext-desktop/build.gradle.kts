8847c9fd7ea0117c0975f3477e608f883856ae29
Natan <xpenatan@gmail.com>
2024-01-19 08:59:22 -0300

    fix path

--- imgui-ext/ext-desktop/build.gradle.kts
+++ imgui-ext/ext-desktop/build.gradle.kts
@@ -1,9 +1,10 @@
 val moduleName = "imgui-ext-desktop"
 
-val windowsFile = "$projectDir/../../imgui/imgui-build/build/c++/libs/windows/ext/imgui64.dll"
-val linuxFile = "$projectDir/../../imgui/imgui-build/build/c++/libs/linux/ext/libimgui64.so"
-val macArmFile = "$projectDir/../imgui-build/build/c++/libs/mac/ext/arm/libimguiarm64.dylib"
-val macFile = "$projectDir/../imgui-build/build/c++/libs/mac/ext/libimgui64.dylib"
+val imguiDir = "$projectDir/../../imgui"
+val windowsFile = "$imguiDir/imgui-build/build/c++/libs/windows/ext/imgui64.dll"
+val linuxFile = "$imguiDir/imgui-build/build/c++/libs/linux/ext/libimgui64.so"
+val macFile = "$imguiDir/imgui-build/build/c++/libs/mac/ext/libimgui64.dylib"
+val macArmFile = "$imguiDir/imgui-build/build/c++/libs/mac/arm/ext/libimguiarm64.dylib"
 
 tasks.jar {
     from(windowsFile)
