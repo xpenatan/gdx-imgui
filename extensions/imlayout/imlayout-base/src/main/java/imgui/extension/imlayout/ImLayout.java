673257e3a40e107345336e1efa85e15b675e596b
Natan <xpenatan@gmail.com>
2024-06-06 22:55:48 -0300

    ImLayout MATCH_WRAP_PARENT

--- extensions/imlayout/imlayout-base/src/main/java/imgui/extension/imlayout/ImLayout.java
+++ extensions/imlayout/imlayout-base/src/main/java/imgui/extension/imlayout/ImLayout.java
@@ -7,7 +7,9 @@ public class ImLayout {
 
     public static final int WRAP_PARENT = WRAP_PARENT();
     public static final int MATCH_PARENT = MATCH_PARENT();
+    public static final int MATCH_WRAP_PARENT = MATCH_WRAP_PARENT();
 
     public static native int WRAP_PARENT();
     public static native int MATCH_PARENT();
+    public static native int MATCH_WRAP_PARENT();
 }
