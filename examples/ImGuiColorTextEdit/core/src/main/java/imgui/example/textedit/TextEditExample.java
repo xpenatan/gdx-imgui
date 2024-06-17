5738422bd2753fd5ae9ca5d84aefc0762de63e8d
Natan <xpenatan@gmail.com>
2024-06-16 22:17:17 -0300

    update example

--- examples/ImGuiColorTextEdit/core/src/main/java/imgui/example/textedit/TextEditExample.java
+++ examples/ImGuiColorTextEdit/core/src/main/java/imgui/example/textedit/TextEditExample.java
@@ -4,15 +4,22 @@ import imgui.ImGui;
 import imgui.example.renderer.ImGuiRenderer;
 import imgui.extension.textedit.LanguageDefinitionId;
 import imgui.extension.textedit.TextEditor;
+import imgui.idl.helper.IDLInt;
 import imgui.idl.helper.IDLString;
 
 public class TextEditExample extends ImGuiRenderer {
 
     private TextEditor editor;
 
+    private IDLInt outLine;
+    private IDLInt outColumn;
+
     @Override
     public void show() {
         super.show();
+
+        outLine = new IDLInt();
+        outColumn = new IDLInt();
 
         editor = new TextEditor();
 
@@ -35,12 +42,12 @@ public class TextEditExample extends ImGuiRenderer {
 
     @Override
     public void renderImGui() {
-//        Coordinates coordinates = editor.GetCursorPosition();
+        editor.GetCursorPosition(outLine, outColumn);
 
         ImGui.Begin("Editor");
 
-//        String text = "\t" + (coordinates.mLine() + 1) + "/" + (coordinates.mColumn() + 1) + " " + editor.GetTotalLines() + " lines | " + (editor.IsOverwrite() ? "Ovr" : "Ins") + " | " + (editor.CanUndo() ? "*" : " ") + " | " + editor.GetLanguageDefinition().mName().c_str();
-//        ImGui.Text(text);
+        String text = "\t" + (outLine.getValue() + 1) + "/" + (outColumn.getValue() + 1) + " " + editor.GetLineCount() + " lines | " + (editor.IsOverwriteEnabled() ? "Ovr" : "Ins") + " | " + (editor.CanUndo() ? "*" : " ") + " | " + editor.GetLanguageDefinitionName().c_str();
+        ImGui.Text(text);
 
         editor.Render("Title");
         ImGui.End();
