89913c1ef319d12c8a82af40e2558db09ccd3d6e
Natan <xpenatan@gmail.com>
2024-07-09 12:30:04 -0300

    update jparser

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,7 @@ object LibExt {
     const val gdxVersion = "1.12.1"
     const val teaVMVersion = "0.10.0"
     const val gdxTeaVMVersion = "-SNAPSHOT"
-    const val jParserVersion = "-SNAPSHOT"
+    const val jParserVersion = "1.0.0-b5"
     const val jUnitVersion = "4.12"
 
     const val exampleUseRepoLibs = false
