aa2039bea3c3f7bfe5ae378c5f2865bf9997de4f
Natan <xpenatan@gmail.com>
2025-03-15 19:47:25 -0300

    Attempt to fix build

--- imgui-ext/ext-core/build.gradle.kts
+++ imgui-ext/ext-core/build.gradle.kts
@@ -17,12 +17,15 @@ java {
 }
 
 val fromClasses = tasks.register<Jar>("fromClasses") {
-    val dependencies = configurations
-        .compileClasspath
-        .get()
-    from((dependencies).map(::zipTree))
+    from(sourceSets.main.get().output)
+    val projectDependencies = configurations["api"].allDependencies
+        .filterIsInstance<ProjectDependency>()
+        .map { it.dependencyProject.tasks.named<Jar>("jar") }
+    dependsOn(projectDependencies)
+    from(projectDependencies.map { zipTree(it.get().archiveFile.get().asFile) })
+
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    dependsOn("assemble")
+    archiveFileName.set("$moduleName.jar") // Optional: Name the output JAR
 }
 
 val sourcesJar = tasks.register<Jar>("sourcesJar") {
@@ -50,7 +53,7 @@ publishing {
                 val dependencyNode = dependenciesNode.appendNode("dependency")
                 dependencyNode.appendNode("groupId", "com.github.xpenatan.jParser")
                 dependencyNode.appendNode("artifactId", "loader-core")
-                dependencyNode.appendNode("version", LibExt.jParserVersion)
+                dependencyNode.appendNode("version", "1.0-SNAPSHOT")
                 dependencyNode.appendNode("scope", "compile")
             }
         }
