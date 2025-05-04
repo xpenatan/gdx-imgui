abb228110750d228e379028a79a448bddccd17d4
Natan <xpenatan@gmail.com>
2025-05-04 16:57:19 -0300

    add jitpack repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ allprojects  {
         mavenCentral()
         maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
         maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
+        maven { url = uri("https://jitpack.io") }
     }
 
     configurations.configureEach {
