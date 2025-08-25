0d18f6755d2b67b51987fa25d5cab6d2ba89509d
Natan <xpenatan@gmail.com>
2025-08-24 21:03:14 -0300

    Update shader

--- gdx/gdx-wgpu-impl/src/main/java/imgui/gdx/ImGuiGdxWGPUImpl.java
+++ gdx/gdx-wgpu-impl/src/main/java/imgui/gdx/ImGuiGdxWGPUImpl.java
@@ -14,7 +14,7 @@ public class ImGuiGdxWGPUImpl implements ImGuiImpl {
     public ImGuiGdxWGPUImpl() {
 
 
-        imguiShader = new WgShaderProgram("imgui", getShaderSource(), "");
+        imguiShader = new WgShaderProgram("imgui", shader_wgsl, "");
 
         System.out.println();
 
@@ -33,62 +33,36 @@ public class ImGuiGdxWGPUImpl implements ImGuiImpl {
         imguiShader.dispose();
     }
 
-
-    private String getShaderSource() {
-        return __shader_frag_wgsl + "\n" + __shader_vert_wgsl;
-    }
-
-    public String __shader_frag_wgsl =
+    public String shader_wgsl =
             "struct VertexOutput {\n" +
             "    @builtin(position) position: vec4<f32>,\n" +
             "    @location(0) color: vec4<f32>,\n" +
             "    @location(1) uv: vec2<f32>,\n" +
             "};\n" +
-            "\n" +
-            "struct Uniforms {\n" +
-            "    mvp: mat4x4<f32>,\n" +
-            "    gamma: f32,\n" +
-            "};\n" +
-            "\n" +
-            "@group(0) @binding(0) var<uniform> uniforms: Uniforms;\n" +
-            "@group(0) @binding(1) var s: sampler;\n" +
-            "@group(1) @binding(0) var t: texture_2d<f32>;\n" +
-            "\n" +
-            "@fragment\n" +
-            "fn main(in: VertexOutput) -> @location(0) vec4<f32> {\n" +
-            "    let color = in.color * textureSample(t, s, in.uv);\n" +
-            "    let corrected_color = pow(color.rgb, vec3<f32>(uniforms.gamma));\n" +
-            "    return vec4<f32>(corrected_color, color.a);\n" +
-            "}\n" +
-            ")";
-
-    public String __shader_vert_wgsl =
             "struct VertexInput {\n" +
             "    @location(0) position: vec2<f32>,\n" +
             "    @location(1) uv: vec2<f32>,\n" +
             "    @location(2) color: vec4<f32>,\n" +
             "};\n" +
-            "\n" +
-            "struct VertexOutput {\n" +
-            "    @builtin(position) position: vec4<f32>,\n" +
-            "    @location(0) color: vec4<f32>,\n" +
-            "    @location(1) uv: vec2<f32>,\n" +
-            "};\n" +
-            "\n" +
             "struct Uniforms {\n" +
             "    mvp: mat4x4<f32>,\n" +
             "    gamma: f32,\n" +
             "};\n" +
-            "\n" +
             "@group(0) @binding(0) var<uniform> uniforms: Uniforms;\n" +
-            "\n" +
+            "@group(0) @binding(1) var s: sampler;\n" +
+            "@group(1) @binding(0) var t: texture_2d<f32>;\n" +
+            "@fragment\n" +
+            "fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {\n" +
+            "    let color = in.color * textureSample(t, s, in.uv);\n" +
+            "    let corrected_color = pow(color.rgb, vec3<f32>(uniforms.gamma));\n" +
+            "    return vec4<f32>(corrected_color, color.a);\n" +
+            "}\n" +
             "@vertex\n" +
-            "fn main(in: VertexInput) -> VertexOutput {\n" +
+            "fn vs_main(in: VertexInput) -> VertexOutput {\n" +
             "    var out: VertexOutput;\n" +
             "    out.position = uniforms.mvp * vec4<f32>(in.position, 0.0, 1.0);\n" +
             "    out.color = in.color;\n" +
             "    out.uv = in.uv;\n" +
             "    return out;\n" +
-            "}\n" +
-            ")";
+            "}\n";
 }
